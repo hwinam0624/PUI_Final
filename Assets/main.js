d12ac29feb76a26594ab32b5dc8a1395c41c1920
Stacie Nam <43346926+hwinam0624@users.noreply.github.com>
2018-11-30 23:55:17 -0500

    Add files via upload

---
+++ Assets/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#ears').on("click", function(){
+        $("#hiddenEars").toggle();
+    });
+});
+$(document).ready(function(){
+    $('#tails').on("click", function(){
+        $("#hiddenTails").toggle();
+    });
+});
+$(document).ready(function(){
+    $('#eyes').on("click", function(){
+        $("#hiddenEyes").toggle();
+    });
+});
---
+++ Assets/main2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#body').on("click", function(){
+        $("#hellyes").toggle();
+    });
+});
+$(document).ready(function(){
+    $('#face').on("click", function(){
+        $("#awesome").toggle();
+    });
+});
+$(document).ready(function(){
+    $('#tails').on("click", function(){
+        $("#meh").toggle();
+    });
+});
